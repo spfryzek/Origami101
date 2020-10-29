d462489ed32c27fc10c4424309332dca847a859d
spfryzek <spfryzek@gmail.com>
2020-10-29 15:19:32 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  return (
+    <div>
+      <center>
+      <h1>Hello StackBlitz!</h1>
+      </center>
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
+}
