1ffbd497cd3a8203a8e84a7f7424eb259b2fd4c8
Sam Fryzek <samfryzek@Sams-MacBook-Pro.local>
2020-11-15 15:42:12 -0500

    updated readme to origami101
    changed App.js to Login.js
    
    created Login css
    
    created general style css for all pages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import App from "./App";
+import Login from "./Login";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Login />, document.getElementById("root"));
