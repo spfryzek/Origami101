440568bbfa2d20ac9d80038723bbcb7efa7ca170
tanyashroff <32010844+tanyashroff@users.noreply.github.com>
2020-12-07 05:45:45 -0500

    Create Grades.js

---
+++ src/components/Grades.js
@@ -0,0 +1,19 @@
+import {Link} from 'react-router-dom';
+import React from 'react';
+import '../css/style.css';
+
+function Grades() {
+    return (
+        <div style={{marginLeft: 2 + "em"}}>
+            <center>
+                <h1>Grades</h1>
+            </center>
+
+            <h3> Courses taken: </h3>
+            Easy Origami: <br />
+            
+        </div>
+    );
+}
+
+export default Grades;
